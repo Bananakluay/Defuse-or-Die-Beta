cbba7ab24d4ed5d6f8f34dd3756b1ac859594b6f
Bananakluay <phatphong.x@gmail.com>
2024-09-07 04:25:40 +0700

    last

--- src/data/instructionData.ts
+++ src/data/instructionData.ts
@@ -1,73 +1,112 @@
 import { Instruction } from "../logic/Instruction";
 
 export const instructionData: Record<number, Instruction[]> = {
-  0: [
-    {
-      action: 'cut', wireColor: 'blue',
-    },
-
-    {
-      action: 'cut', wireColor: 'red',
-      condition: {
-        serial: { contains: 'A' },
-        time: {type:'withIn' , value: 5}
-      }
-    },
-    {
-      action: 'turnOn', switchName: 'SB1',
-      condition: {
-        time: { type: 'at', value: 5 }
-      }
-    },
-    {
-      action: 'pull', fuseName: 'F1',
-      condition: {
-        time: { type: 'withIn', value: 5 }
-      }
-    },
-    {
-      action: 'turnOn', switchName: 'SB2',
-      condition: {
-        time: { type: 'at', value: 5 }
-      }
-    },
-  ],
   1: [
-    { action: 'cut', wireColor: 'red' },
-    {
-      action: 'hold', pushButtonName: "PB",
-      condition: {
-        time: { type: 'at', value: 5 }
-      }
-    }
+    { action: 'press', pushButtonName: 'PB' }
   ],
-  150: [
-    { action: 'pull', fuseName: 'F1' },
+  3: [
+    { action: 'pull', eCompName: 'C1' }
+  ],
+  4: [
+    { action: 'keyPress', keyNum: '101' },
+    { action: 'turnOn', switchName: 'SB2', condition: { time: { at: 1 } } }
+  ],
+  6: [
+    { action: 'pull', eCompName: 'C2' }
+  ],
+  9: [
+    { action: 'press', pushButtonName: 'PB' },
+    { action: 'press', pushButtonName: 'PB' }
+  ],
+  25: [
+    { action: 'pull', fuseName: 'Fuse 2' },
+    { action: 'press', pushButtonName: 'PB', condition: { time: { within: 9 } } }
+  ],
+  37: [
+    { action: 'cut', wireColor: 'red' },
+    { action: 'press', pushButtonName: 'PB' },
+    { action: 'turnOn', switchName: 'SB1' }
+  ],
+  46: [
+    { action: 'pull', batteryName: 'Battery 1' },
+    { action: 'press', pushButtonName: 'PB', count: 4 }
+  ],
+  59: [
+    { action: 'pull', eCompName: 'com_transistor_brown' },
+    { action: 'keyPress', keyNum: '173' },
+    { action: 'turnOff', switchName: 'SB2', condition: { time: { at: 0 } } }
+  ],
+  78: [
+    { action: 'keyPress', keyNum: '0106' },
+    { action: 'press', pushButtonName: 'PB' }
+  ],
+  93: [
+    { action: 'pull', eCompName: 'com_transistor_blue' },
+    { action: 'press', pushButtonName: 'PB', condition: { time: { within: 8 } } }
+  ],
+  112: [
+    { action: 'pull', eCompName: 'chip-main' },
+    { action: 'press', pushButtonName: 'PB' },
+    { action: 'cut', wireColor: 'yellow' }
+  ],
+  200: [
+    { action: 'press', pushButtonName: 'PB', count: 2 },
+    { action: 'pull', eCompName: 'com_capacitor2', condition: { serial: { endsWith: 'A' } } },
+    { action: 'keyPress', keyNum: '19101', condition: { serial: { notEndsWith: 'A' } } },
+    { action: 'keyPress', keyNum: '01101' },
+    { action: 'turnOff', switchName: 'SB1' }
+  ],
+  375: [
+    { action: 'pull', fuseName: 'Fuse 3' },
+    { action: 'cut', wireColor: 'blue', condition: { time: { within: 5 } } },
+    { action: 'press', pushButtonName: 'PB' }
+  ],
+  692: [
+    { action: 'keyPress', keyNum: '2547' },
+    { action: 'pull', eCompName: 'com_transistor_black' },
+    { action: 'press', pushButtonName: 'PB', condition: { time: { at: 8 } } }
+  ],
+  823: [
+    { action: 'pull', eCompName: 'chip-small' },
+    { action: 'keyPress', keyNum: '8462' },
+    { action: 'press', pushButtonName: 'PB' },
+    { action: 'turnOn', switchName: 'SB1' }
+  ],
+  1124: [
+    { action: 'pull', batteryName: 'Battery 2' },
+    { action: 'cut', wireColor: 'pink', condition: { time: { at: 5 } } },
+    { action: 'press', pushButtonName: 'PB', count: 2 },
+    { action: 'keyPress', keyNum: '458' },
+    { action: 'turnOff', switchName: 'SB2' }
+  ],
+  3333: [
+    { action: 'press', pushButtonName: 'PB' },
+    { action: 'keyPress', keyNum: '5322' },
+    { action: 'pull', fuseName: 'Fuse 1', condition: { serial: { endsWith: 'B' } } },
+    { action: 'turnOff', switchName: 'SB1', condition: { serial: { notEndsWith: 'B' } } },
+    { action: 'cut', wireColor: 'green', condition: { time: { within: 4 } } },
+    { action: 'keyPress', keyNum: '2235' },
+    { action: 'press', pushButtonName: 'PB' }
+  ],
+  5423: [
+    { action: 'pull', eCompName: 'com_capacitor3' },
+    { action: 'keyPress', keyNum: '2345' },
+    { action: 'turnOn', switchName: 'SB1' },
+    { action: 'cut', wireColor: 'yellow' },
+    { action: 'press', pushButtonName: 'PB' }
+  ],
+  6584: [
+    { action: 'keyPress', keyNum: '2580' },
+    { action: 'pull', eCompName: 'chip-big', condition: { time: { within: 5 } } },
     { action: 'keyPress', keyNum: '1' },
-    { action: 'keyPress', keyNum: '5' },
-    { action: 'keyPress', keyNum: '2' },
-    { action: 'keyPress', keyNum: '1' }
-  ],
-  220: [
     { action: 'turnOff', switchName: 'SB1' },
-    { action: 'cut', wireColor: 'red' },
-    { action: 'cut', wireColor: 'green' }
+    { action: 'press', pushButtonName: 'PB', count: 2 }
   ],
-  440: [
-    { action: 'turnOn', switchName: 'SB2' },
-    { action: 'cut', wireColor: 'pink' },
-    { action: 'turnOn', switchName: 'SB1' }
-  ],
-  550: [
-    { action: 'pull', fuseName: 'F3' },
-    { action: 'cut', wireColor: 'white' },
-    { action: 'turnOn', switchName: 'SB1' }
-  ],
-  660: [
+  8796: [
     { action: 'pull', eCompName: 'C1' },
-    { action: 'cut', wireColor: 'pink' }
-  ],
-  770: [
-    { action: 'keyPress', keyNum: '1' }
+    { action: 'press', pushButtonName: 'PB', count: 3 },
+    { action: 'cut', wireColor: 'red', condition: { time: { at: 2 } } },
+    { action: 'press', pushButtonName: 'PB', count: 4 },
+    { action: 'turnOff', switchName: 'SB2' }
   ]
 };
